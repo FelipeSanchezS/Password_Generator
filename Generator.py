1c5cedf722dda22501be19a4e758576b7560ea0d
Luis Felipe Sanchez Sanchez <155125471+FelipeSanchezS@users.noreply.github.com>
2024-12-27 01:15:32 +0000

    Esta es una V2 con condicionalesa dentro

--- Generator.py
+++ Generator.py
@@ -1,13 +1,21 @@
-import string
-import random
-
-longitud = int(input("Ingrese la longitud de la contraseña: "))
+import string;
+import random;
 
 #Agregamos todso los simbolos, letras y numeros de odnde se creara la contraseña 
 caracteres = string.ascii_letters + string.digits + string.punctuation
 
-#Elije caracteres aleatorios que esten dentro de la variable caracteres
-contraseña = "".join(random.choice(caracteres)
-for i in range(longitud))
+while True:
+    try:
+        #Variable para la longitud
+        longitud = int(input("Ingrese la longitud de la contraseña: "));
 
-print("La contraseña generada por el sistema es: "+contraseña)
+        if (longitud <= 7 ):
+            print("La longitud de los caracteres debe ser mayor a 8, intente nuevamente");
+
+        else:
+            #Elije caracteres aleatorios que esten dentro de la variable caracteres
+            contraseña = "".join(random.choice(caracteres)for i in range(longitud));
+            print("La contraseña generada por el sistema es: "+ contraseña)
+
+    except ValueError:
+        print("Ingrese la longitud nuevamente")
